06d0644e8f375f8f7f974eb68d6a9ab92260d820
Irwansyah <irwansyah@gmail.com>
2016-04-20 19:32:41 +0700

    Can display login and buy tickets screen

--- src/index.js
+++ src/index.js
@@ -1,14 +1,51 @@
 import angular from 'angular';
+import Firebase from 'firebase';
 
-var phonecatApp = angular.module('phonecatApp', []);
+import  'angular-route';
 
-phonecatApp.controller('PhoneListCtrl', function ($scope) {
-  $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
-  ];
+var phonecatApp = angular.module('MilesEvent', ['ngRoute']);
+
+phonecatApp.config(['$routeProvider', ($routeProvider) =>{
+	$routeProvider
+		.when('/login', {
+			templateUrl: '/partials/login.html',
+			controller: 'LoginController'
+		})
+		.when('/buy-tickets', {
+			templateUrl: '/partials/buy-tickets.html',
+			controller: "BuyTicketsController"
+		})
+		.otherwise({
+			redirectTo: '/login'
+		})	
+}])
+
+phonecatApp.controller('LoginController',  ($scope, $location) => {
+
+	$scope.firebaseRef = new Firebase('https://brilliant-heat-6714.firebaseio.com')
+
+	$scope.facebookLogin = () =>{
+
+		$scope.firebaseRef.authWithOAuthPopup("facebook", (error, authData)=>{
+			if(error){
+				console.log("Facebook login failed", error)
+			}
+			else{
+				$location.path('/buy-tickets')
+			}
+		})
+	}
+});
+
+phonecatApp.controller('BuyTicketsController',  ($scope) => {
+	$scope.buyTicket = (ticketId) =>{
+		switch(ticketId){
+			case 1: 
+				console.log("Ticket1 ")
+				break;
+			case 2: 
+				console.log("Ticket2 ")
+				break;
+		}
+	}
 });
